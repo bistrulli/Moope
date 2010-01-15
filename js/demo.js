bda6e39b8a28cad208a1b84e99da0936a6804dd2
Emilio <bistrulli@gmail.com>
2010-01-15 19:39:35 +0100

    Signed-off-by: Emilio <bistrulli@gmail.com>

--- js/demo.js
+++ js/demo.js
@@ -1,11 +1,11 @@
-var p1=new pe_prop("icons/address-book.png", "name","content",true);
-var p2=new pe_prop("icons/address-book.png", "twitter","@santennio",true);
-var p3=new pe_prop("icons/address-book.png", "fb","santennio",true);
+var p1=new pe_prop("../images/address-book.png", "name","content",true);
+var p2=new pe_prop("../images/address-book.png", "twitter","@santennio",true);
+var p3=new pe_prop("../images/address-book.png", "fb","santennio",true);
 var ar=new Array();
 ar.push(p1);
 ar.push(p2);
 
-var item1=new pe_item("icons/address-book.png", ar,"100px");
+var item1=new pe_item("../images/address-book.png", ar,"100px");
 item1.addProps(p3);
 
 /*var p12=new pe_prop("icons/address-book.png", "name","content",true);
